99de52b674eee0f49d8813e3f500979597ede5ff
Olivier PERDRIX <olivierperdrix@live.fr>
2024-12-02 13:59:38 +0100

    improve design

--- vulnerable/frontend/src/components/Navbar.tsx
+++ vulnerable/frontend/src/components/Navbar.tsx
@@ -7,36 +7,42 @@ const Navbar = () => {
 
   return (
     <div className="navbar bg-base-100 shadow-lg">
-      <div className="flex-1">
+      <div className="navbar-start">
         <Link to="/" className="btn btn-ghost normal-case text-xl">
           BlogApp
         </Link>
       </div>
-      <div className="flex-none">
+      <div className="navbar-end">
         <ul className="menu menu-horizontal px-1 gap-2">
           <li>
-            <Link className="btn" to="/">Home</Link>
+            <Link className="" to="/">Home</Link>
           </li>
           {user ? (
             <>
               {user.role === "admin" && (
                 <li>
-                  <Link className="btn" to="/admin">Admin</Link>
+                  <Link className="" to="/admin">Admin</Link>
                 </li>
               )}
               <li>
-                <button className="btn btn-error" onClick={logout}>
-                  Logout
-                </button>
+                <details>
+                  <summary>Bonjour {user.username} (role {user.role})</summary>
+                  <ul className="bg-base-100 rounded-t-none p-2">
+                    <li><button className="btn btn-error btn-sm" onClick={logout}>
+                      Logout
+                    </button></li>
+
+                  </ul>
+                </details>
               </li>
             </>
           ) : (
             <>
               <li>
-                <Link className="btn" to="/login">Login</Link>
+                <Link className="" to="/login">Login</Link>
               </li>
               <li>
-                <Link className="btn" to="/register">Register</Link>
+                <Link className="" to="/register">Register</Link>
               </li>
             </>
           )}
